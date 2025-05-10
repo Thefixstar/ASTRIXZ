8f659266361ad0d9e8256e8a54e517a68b38e715
Thefixstar <thefixstarx@gmail.com>
2025-05-10 15:13:02 +0200

    Primer commit del bot

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const config = require('../config.json');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.commands = new Collection();
+
+// Cargar comandos
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+// Cargar eventos
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  client.on(event.name, (...args) => event.execute(...args, client));
+}
+
+client.login(config.token);
+client.once('ready', () => {
+    console.log(`✅ Bot conectado como ${client.user.tag}`);
+  });
